671600206f3a74b999331c98fd279c3f2eaaeb68
kat <kat@pop-os.localdomain>
2023-11-14 16:10:11 +0000

    Add functionality for search phonebook by name.

--- src/App.jsx
+++ src/App.jsx
@@ -4,10 +4,21 @@ import Entry from './components/Entry';
 
 export default function App() {
   const [entries, setEntries] = useState([
-    { name: 'Pingu', number: '02086876000' }
+    { name: 'Pingu', number: '02086876000' },
+    { name: 'Piplup', number: '07066775792' },
+    { name: 'Wheezy', number: '07830494624' },
+    { name: 'Tuxedosam', number: '07750295291' },
+    { name: 'Tux', number: '07027167775' },
+    { name: 'Pinga', number: '07034452515' },
   ]);
   const [newName, setNewName] = useState('');
   const [newNumber, setNewNumber] = useState('');
+  const [results, setResults] = useState('');
+
+  function handleSearchChange(event) {
+    setResults(entries.filter((entry) => 
+      entry.name.toLowerCase().includes(event.target.value.toLowerCase())));
+  }
 
   function addEntry(event) {
     event.preventDefault();
@@ -37,30 +48,47 @@ export default function App() {
 
   return (
     <div>
-      <h2>Phonebook</h2>
-      <form onSubmit={addEntry}>
-        <div>
-          <label htmlFor="name">Name: </label>
-          <input 
-            id="name" value={newName} onChange={handleNameChange} required>
-          </input>
-          <br />
-          <label htmlFor="number">Number: </label>
-          <input 
-            id="number" value={newNumber} onChange={handleNumberChange} 
-              required>
-          </input>
-        </div>
-        <div>
-          <button type="submit">add</button>
-        </div>
-      </form>
-      <h2>Numbers</h2>
-      <ul>
-        {entries.map((person) => 
-          <Entry key={person.name} entry={person} />
-        )}
-      </ul>
+      <div>
+        <h1>Phonebook</h1>
+      </div>
+      <div>
+        <h2>Search for an entry by name</h2>
+        <label htmlFor="search">Search: </label>
+        <input id="search" onChange={handleSearchChange}></input>
+        <ul>
+          {results && results.length !== entries.length ? 
+            results.map((result) => <Entry key={result.name} entry={result} />)
+              : ''}
+        </ul>
+      </div>
+      <div>
+        <h2>Add an entry</h2>
+        <form onSubmit={addEntry}>
+          <div>
+            <label htmlFor="name">Name: </label>
+            <input 
+              id="name" value={newName} onChange={handleNameChange} required>
+            </input>
+            <br />
+            <label htmlFor="number">Number: </label>
+            <input 
+              id="number" value={newNumber} onChange={handleNumberChange} 
+                required>
+            </input>
+          </div>
+          <div>
+            <button type="submit">add</button>
+          </div>
+        </form>
+      </div>
+      <div>
+        <h2>Numbers</h2>
+        <ul>
+          {entries.map((person) => 
+            <Entry key={person.name} entry={person} />
+          )}
+        </ul>
+      </div>
     </div>
   );
 }
